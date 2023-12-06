24273fc2f96a2e32fe47df76202fffdfbf098405
joseppino <joseph.lloyd17@hotmail.com>
2023-12-06 17:03:41 +0000

    some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
+<script lang="ts">
+    let country: any = {name: 'Latvia', code: 'LV'};
+</script>
+
 <main>
     <h1>Flaggle</h1>
     <h4>A flag guessing game</h4>
+    <img class="flag-image" src={`https://flagpedia.net/data/flags/h120/${country.code.toLowerCase()}.webp`} alt="">
 </main>
+
+<style>
+    .flag-image {
+        max-width: 150px;
+    }
+</style>
